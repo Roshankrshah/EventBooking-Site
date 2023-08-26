d2b4e160092a47ffc5029e9d6b270dbdd0f1d5b6
Roshan <shahroshan0720@gmail.com>
2023-08-26 14:15:46 +0530

    graphql initial setup

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    res.send('Building GraphQL server');
-})
+app.use('/graphql', graphqlHttp.graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
-app.listen(3003,()=>{
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Wedding', 'Cooking', 'Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3003, () => {
     console.log(`Server listening at http://localhost:3003`);
 });
