25c9a18b49de98c4084bdd9fbf2f80288956fe22
Roshan <shahroshan0720@gmail.com>
2023-08-28 11:21:48 +0530

    add logout

--- frontend/events.js
+++ frontend/events.js
@@ -1,5 +1,14 @@
 
 if(localStorage.getItem('token')){
+    document.querySelector('.logoutBtn').classList.remove('disabled');
+    document.querySelector('.logoutBtn').setAttribute('aria-disabled','false');
     document.querySelector('.booking').classList.remove('disabled');
-    document.querySelector('.booking').setAttribute('aria-disabled',false);
+    document.querySelector('.booking').setAttribute('aria-disabled','false');
 }
+
+const logoutBtn = document.querySelector('.logoutBtn');
+
+logoutBtn.addEventListener('click',()=>{
+    localStorage.clear();
+    window.location.replace("http://127.0.0.1:5500/frontend/login.html");
+})
