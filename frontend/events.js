8a05b694fb3f5899e828bfb735a3d6d8a494a9ed
Roshan <shahroshan0720@gmail.com>
2023-08-28 13:52:58 +0530

    modal added

--- frontend/events.js
+++ frontend/events.js
@@ -7,8 +7,20 @@ if(localStorage.getItem('token')){
 }
 
 const logoutBtn = document.querySelector('.logoutBtn');
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.container');
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector(".close-btn");
 
 logoutBtn.addEventListener('click',()=>{
     localStorage.clear();
     window.location.replace("http://127.0.0.1:5500/frontend/login.html");
+});
+
+btn.addEventListener('click',()=>{
+    modal.classList.add('open-modal')
 })
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
+
