dc4465f905bc2ce0b15909f3ccd527c0156b799a
Roshan <shahroshan0720@gmail.com>
2023-08-28 22:17:40 +0530

    cancel booking setup

--- frontend/bookings.js
+++ frontend/bookings.js
@@ -1,7 +1,7 @@
-const listContainer = document.querySelector('.booked_events_list');
+const listContainer = document.querySelector('.bookings_list');
 
 const requestBody = {
-    query:`
+    query: `
         query{
             bookings{
                 _id
@@ -17,9 +17,56 @@ const requestBody = {
 
 const token = localStorage.getItem('token');
 
+fetch('http://localhost:3003/graphql', {
+    method: 'POST',
+    body: JSON.stringify(requestBody),
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+    }
+})
+    .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+            throw new Error('Failed!');
+        }
+        return res.json();
+    })
+    .then(resData => {
+        console.log(resData);
+        const innerEle = resData.data.bookings.map(booking => {
+            return `
+                <li class="bookings_item">
+                    <div class="bookings_item-data">
+                        ${booking.event.title}<span> - </span> ${new Date(booking.createdAt).toLocaleDateString()}
+                    </div>
+                    <div class="bookings_item-actions">
+                        <button class="cancel_booking-btn" onClick = "deleteBooking('${booking._id}')">Cancel</button>
+                    </div>
+                </li>
+                `;
+        }).join('');
+
+        listContainer.innerHTML = innerEle;
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+const deleteBooking = bookingId => {
+    console.log(bookingId);
+    const deleteBody = {
+        query: `
+            mutation{
+                cancelBooking(bookingId: "${bookingId}"){
+                    _id
+                    title
+                }
+            }`
+    };
+
     fetch('http://localhost:3003/graphql', {
         method: 'POST',
-        body: JSON.stringify(requestBody),
+        body: JSON.stringify(deleteBody),
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + token
@@ -33,16 +80,9 @@ const token = localStorage.getItem('token');
         })
         .then(resData => {
             console.log(resData);
-            const listEle = document.createElement('li');
-            listEle.classList.add('booked_list-item');
-            const innerEle = resData.data.bookings.map(booking=>{
-                return `
-                    <p>${booking.event.title}<span> - </span> ${new Date(booking.createdAt).toLocaleDateString()}</p>
-                `;
-            }).join('');
-            
-            listContainer.innerHTML = innerEle;
         })
         .catch(err => {
             console.log(err);
         })
+    location.reload();
+}
